27d6b4cf86e6a6c37c0f85bd3e7dca811038499f
LsXxPikachuxX <xur1@bxscience.edu>
2016-12-29 11:04:05 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,1 +1,19 @@
-
+//Currently learning how to do this. Will post finished file/files.
+using System.Windows.Forms;
+  
+class MyFirstForm : Form
+{
+    public MyFirstForm()
+    {
+         this.Text = "My First Form";
+    }
+}
+    
+class MyFirstApp
+{
+     static void Main()
+     {
+          Form form = new myFirstForm();
+          Application.Run(form);
+     }
+}
