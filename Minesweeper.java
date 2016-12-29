4b5ff87001b9b99a8b18bc6eb37285fcd35ed134
Xurya <xur1@bxscience.edu>
2016-12-29 18:04:42 -0500

    Update Minesweeper.java

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,5 +1,17 @@
+//Note: we mite have to make a superclass(for repeating the thing over and over again? idk)
+import java.util.Scanner;
+
 public class Minesweeper{
   public static void main(String[] args){
-    //add code here ;)
+    Scanner scanner = new Scanner(System.in);
+    //add code here :)
   }
+  
+  //get the 2d grid based on user input from main method
+  public static int[][] getBoard(int n){
+    int[][] grid = new int[n][n];
+    return grid;
+  }
+  
+  
 }
