69cb47cbe3013bb7c2628cb6c02de80d01804ccd
The Ideal is Within 2D <saggo.guilty@gmail.com>
2017-01-03 20:25:33 -0500

    Update Minesweeper.java

--- Minesweeper.java
+++ Minesweeper.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 public class Minesweeper{
   public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
-    //add code here :)
+    System.out.print("Enter the length of the square mine field");
+    int l = scanner.nextInt();
+    int[][] bombField = getBombs(getBoard(l),((int)(l*l/10.0)));
   }
   
   //get the 2d grid based on user input from main method
@@ -14,8 +16,21 @@ public class Minesweeper{
   }
 
   //get the bomb grid based on number of bombs
+  //Made it so that the 1 would be the x
+  //Sorta made it really long tell me if u have a better way to do this
   public static int[][] getBombs(int[][] grid,int numBombs){
-    //add code here :)
+    int[][] ans = new int[grid.length][grid[0].length];
+    for(int i = 0; i<numBombs; i++){
+      int row = (int)(Math.random()*(grid.length-1));
+      int col = (int)(Math.random()*(grid[0].length-1));
+      if(ans[row][col]==0){
+        ans[row][col]=1;
+      }else{
+        i--;
+      }
+    }
+    return ans;
   }
+  
   
 }
